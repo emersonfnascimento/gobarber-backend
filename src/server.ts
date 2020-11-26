0071a45c726e5c2b7a3a572aa7464e8b10970112
Emerson Freitas <emersonfreitasn@gmail.com>
2020-11-25 20:43:47 -0500

    handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express'
-import routes from './routes/index';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
+import routes from './routes/index';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database'
 
@@ -12,6 +14,22 @@ const app = express()
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  });
+});
 
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333!')
